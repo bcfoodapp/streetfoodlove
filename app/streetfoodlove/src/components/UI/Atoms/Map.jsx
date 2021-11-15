f024ae48119dd61cb557e2aa3e1ff8fd1f9d7d00
czhou578 <colizu2020@gmail.com>
2021-11-14 17:48:03 -0800

    created react-app

---
+++ app/streetfoodlove/src/components/UI/Atoms/Map.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+export default function Map(props) {
+  <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+
+  </MapContainer>
+}
