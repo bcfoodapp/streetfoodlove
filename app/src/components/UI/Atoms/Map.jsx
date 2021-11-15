404369f465535dc163ca3e5499b9e7764f0d575c
czhou578 <colizu2020@gmail.com>
2021-11-15 15:51:34 -0800

    commented out two lines

--- app/src/components/UI/Atoms/Map.jsx
+++ app/src/components/UI/Atoms/Map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export default function Map(props) {
   const position = [51.505, -0.09]
