cdd8e7f61854c6fe9df9c54f6ba4a23cd3c142e1
Makoto <makoto@emurasoft.com>
2021-11-17 19:06:53 -0800

    simple API output

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,22 @@
 import Map from './components/UI/Atoms/Map';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {vendor} from './API';
 
 function App(): React.ReactElement {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+	// This is a temporary test
+	const [text, setText] = useState('loading');
+	useEffect(() => {
+		vendor("e72ac985-3d7e-47eb-9f0c-f8e52621a708")
+			.then(obj => setText(JSON.stringify(obj, null, 2)));
+	}, []);
+
+	return (
+		<div className="App">
+      <pre>
+        {text}
+      </pre>
+		</div>
+	);
 }
 
 export default App;
