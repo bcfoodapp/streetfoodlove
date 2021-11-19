aa4ceb5019220a1ae0a15e5cf813010e27143205
Makoto <makoto@emurasoft.com>
2021-11-18 18:47:39 -0800

    add vendor content

---
+++ app/src/api.ts
@@ -0,0 +1,21 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
+
+export interface Vendor {
+  ID: string;
+  BusinessAddress: string;
+  Name: string;
+  Phone: string;
+}
+
+const encode = encodeURIComponent;
+
+export const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8080'}),
+  endpoints: builder => ({
+    getVendor: builder.query<Vendor, string>({
+      query: (id) => `/vendors/${encode(id)}`,
+    }),
+  }),
+});
+
+export const {useGetVendorQuery} = apiSlice;
