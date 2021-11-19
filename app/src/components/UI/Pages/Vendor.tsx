5709dd0faa178c52228942750acf73f670efe1dd
Makoto <makoto@emurasoft.com>
2021-11-18 17:40:59 -0800

    get vendor in vendor page

--- app/src/components/UI/Pages/Vendor.tsx
+++ app/src/components/UI/Pages/Vendor.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import {useGetVendorQuery} from '../../../api';
 
 export function Vendor(): React.ReactElement {
-	const params = useParams();
-	console.log(params.ID);
-	return <p>vendor</p>;
+  const params = useParams();
+  const query = useGetVendorQuery(params.ID as string);
+
+  let content;
+  if (query.isLoading) {
+    content = <p>loading</p>;
+  } else if (query.isError) {
+    content = <>
+      <p>{'status: ' + (query.error as any).status}</p>
+      <p>{(query.error as any).error}</p>
+    </>;
+  } else if (query.isSuccess) {
+    content = <pre>{JSON.stringify(query.data, null, 2)}</pre>
+  }
+
+  return <>
+    {content}
+  </>;
 }
