d4e13d40c09faedb86f5a0724299541374f4d45a
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2021-11-20 14:30:51 -0800

    Modified Vendor with TextArea (#26)

--- app/src/components/UI/Atoms/Button/Buttons.tsx
+++ app/src/components/UI/Atoms/Button/Buttons.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import classes from "./button.module.css"
 import { Button } from 'semantic-ui-react'
 
+/**
+ * This file assigns the respective classes and styling classes to all the different buttons on this app.  
+ */
+
 export default function Buttons(props): React.ReactElement {
 
   let name = ''
@@ -13,13 +17,14 @@ export default function Buttons(props): React.ReactElement {
   else if (props.apply) name = classes.Apply
   else if (props.getstarted) name = classes.getStarted
   else if (props.logout) name = classes.Logout
+  else if (props.cancel) name = classes.Cancel
   else throw new Error('Invalid Prop')
 
   return (
-    <div>
-       <Button floated="right" className={classes.Button + ' ' + name}>
-         <span>{props.children}</span>
-       </Button>
-    </div>
+    <React.Fragment>
+      <Button className={classes.Button + ' ' + name}>
+        <span>{props.children}</span>
+      </Button>
+    </React.Fragment>
   )
 }
--- app/src/components/UI/Pages/Vendor.tsx
+++ app/src/components/UI/Pages/Vendor.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useParams} from 'react-router-dom';
 import {useGetVendorQuery} from '../../../api';
+import { Form, TextArea, Container, Button } from 'semantic-ui-react'
+import Buttons from '../Atoms/Button/Buttons';
+import './vendor.css'
 
 export function Vendor(): React.ReactElement {
   const params = useParams();
   const query = useGetVendorQuery(params.ID as string);
+  const [textAreaInput, setTextAreaInput] = useState('');
 
   let content;
   if (query.isLoading) {
@@ -23,7 +27,29 @@ export function Vendor(): React.ReactElement {
     </>;
   }
 
+  const handleChange = (e) => {
+    setTextAreaInput(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setTextAreaInput('')
+  }
+
   return <>
     {content}
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <TextArea 
+            placeholder='Write Review here...' 
+            style={{minHeight: 60, minWidth: 500}}
+            value={textAreaInput}
+            onChange={handleChange}
+          />
+        <Container>
+          <Buttons cancel>Cancel</Buttons>
+          <Buttons submit>Submit</Buttons>
+        </Container>
+        </Form>
+      </Container>
   </>;
 }
