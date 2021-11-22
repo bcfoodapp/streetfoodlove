dcf39cc8f4c0a3b29d5934ff1031b53c53095bcb
czhou578 <colizu2020@gmail.com>
2021-11-22 09:54:55 -0800

    installed semantic-ui-css stylesheets, webpack issue needs fixing

--- app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
+++ app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Container, Search, Icon } from "semantic-ui-react";
+import { Container, Input, Icon } from "semantic-ui-react";
 
 export const SearchBox = (props) => {
-  const element = <Icon name="search" size="small" />
 
   return (
     <Container>
-      <Search
-        size={"large"}
-        showNoResults={true}
-        icon={element}
+      <Input
+        icon="search"
+        placeholder="Se..."
+        size={"huge"}
       />
     </Container>
   )
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
