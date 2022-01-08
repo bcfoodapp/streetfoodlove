6b2a9101e5e95e92b5c89108fb13c99b6f29d5d1
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2022-01-07 17:10:07 -0800

    Create webpack.yml (#45)
    * Create webpack.yml
    
    * deleted strategy section and added all branch check
    
    * npx prettier
    
    * changed prettier options
    
    * testing
    
    * prettier on whole directory
    
    * updated permissions
    
    * test after changing credentials
    
    * added github token
    
    * test
    
    * updated version
    
    * changed directory of path for webpack build
    
    * testing
    
    * change directory
    
    * test
    
    * test
    
    * test
    
    * change directory path

--- app/src/components/UI/Pages/Login.tsx
+++ app/src/components/UI/Pages/Login.tsx
@@ -1,31 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { Container, Form, Checkbox } from "semantic-ui-react";
 import Buttons from "../Atoms/Button/Buttons";
 import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
 import styles from "./login.module.css";
 import { Grid } from "semantic-ui-react";
-import {Credentials, useNewTokenMutation} from '../../../api';
-import {setToken, useAppDispatch, useAppSelector} from '../../../store';
-import {useNavigate} from 'react-router-dom';
+import { Credentials, useNewTokenMutation } from "../../../api";
+import { setToken, useAppDispatch, useAppSelector } from "../../../store";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Displays the Login element in the login page
  */
 
 export default function Login(): React.ReactElement {
-  const [credentials, setCredentials] = useState<Credentials>({Password: '', Username: ''});
+  const [credentials, setCredentials] = useState<Credentials>({
+    Password: "",
+    Username: "",
+  });
   const [newToken] = useNewTokenMutation();
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const error = useAppSelector(state => state.root.error);
+  const error = useAppSelector((state) => state.root.error);
 
   const onSubmit = async () => {
     try {
       const token = await newToken(credentials).unwrap();
       dispatch(setToken(token));
       navigate(-1);
-    } catch(e) {}
-  }
+    } catch (e) {}
+  };
 
   return (
     <>
@@ -43,16 +46,20 @@ export default function Login(): React.ReactElement {
                     placeholder="Username"
                     width={7}
                     value={credentials.Username}
-                    onChange={(_, {value}) => setCredentials({...credentials, Username: value})}
+                    onChange={(_, { value }) =>
+                      setCredentials({ ...credentials, Username: value })
+                    }
                   />
                 </Form.Field>
                 <Form.Field>
                   <Form.Input
                     placeholder="Password"
                     width={7}
-                    type='password'
+                    type="password"
                     value={credentials.Password}
-                    onChange={(_, {value}) => setCredentials({...credentials, Password: value})}
+                    onChange={(_, { value }) =>
+                      setCredentials({ ...credentials, Password: value })
+                    }
                   />
                 </Form.Field>
                 {/* TODO this belongs in the create account form */}
@@ -65,7 +72,7 @@ export default function Login(): React.ReactElement {
                   </Buttons>
                 </Container>
                 {/* Temporary error output */}
-                <pre>{error ? error.toString() : ''}</pre>
+                <pre>{error ? error.toString() : ""}</pre>
               </Form>
             </Grid.Row>
           </Grid.Column>
