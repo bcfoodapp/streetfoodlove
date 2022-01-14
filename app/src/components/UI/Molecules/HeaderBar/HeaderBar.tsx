3e429e75a843f2dbddc215758cb4b71d170d584e
czhou578 <colizu2020@gmail.com>
2022-01-14 12:00:58 -0800

    npx prettier

--- app/src/components/UI/Molecules/HeaderBar/HeaderBar.tsx
+++ app/src/components/UI/Molecules/HeaderBar/HeaderBar.tsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
  */
 
 interface Props {
-  signUp?: boolean,
+  signUp?: boolean;
 }
 
 export default function HeaderBar(props: Props): React.ReactElement {
--- app/src/components/UI/Pages/Signup.tsx
+++ app/src/components/UI/Pages/Signup.tsx
@@ -1,56 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { Form, Container, Button } from 'semantic-ui-react'
+import { Form, Container, Button } from "semantic-ui-react";
 import Buttons from "../Atoms/Button/Buttons";
-import HeaderBar from '../Molecules/HeaderBar/HeaderBar'
-import styles from './signup.module.css'
+import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
+import styles from "./signup.module.css";
 
-export default function Signup():React.ReactElement {
-
-  const storeUserInfo = () => {
-
-  }
+export default function Signup(): React.ReactElement {
+  const storeUserInfo = () => {};
 
   return (
     <Container className={styles.signUpWrapper}>
-      <HeaderBar signUp={false}/>
+      <HeaderBar signUp={false} />
       <h1>Sign Up Form (user account)</h1>
       <Form className={styles.form}>
         <Form.Input
           // error={{ content: 'Please enter your first name', pointing: 'below' }}
           fluid
-          label='First name'
-          placeholder='First name'
-          id='form-input-first-name'
+          label="First name"
+          placeholder="First name"
+          id="form-input-first-name"
           required
           width={5}
         />
         <Form.Input
           // error='Please enter your last name'
           fluid
-          label='Last name'
-          placeholder='Last name'
+          label="Last name"
+          placeholder="Last name"
           required
           width={5}
         />
         <Form.Input
           // error='Please enter your last name'
           fluid
-          label='Username'
-          placeholder='Username'
+          label="Username"
+          placeholder="Username"
           required
           width={5}
         />
 
-          <Form.Input
+        <Form.Input
           // error='Please enter your last name'
           fluid
-          label='Password'
-          placeholder='Password'
+          label="Password"
+          placeholder="Password"
           required
           width={5}
         />
         <Form.Checkbox
-          label='I agree to the Terms and Conditions'
+          label="I agree to the Terms and Conditions"
           // error={{
           //   content: 'You must agree to the terms and conditions',
           //   pointing: 'left',
@@ -59,8 +56,10 @@ export default function Signup():React.ReactElement {
         />
       </Form>
       <Container className={styles.btnContainer}>
-        <Buttons clicked={() => storeUserInfo} color="green" signup>Sign Up</Buttons>
+        <Buttons clicked={() => storeUserInfo} color="green" signup>
+          Sign Up
+        </Buttons>
       </Container>
     </Container>
-  )
+  );
 }
