1231afa4d17f9adaade20af702dc213996c23eba
czhou578 <colizu2020@gmail.com>
2022-01-15 11:21:56 -0800

    vendorappform design done

--- app/src/components/UI/Pages/VendorAppForm.tsx
+++ app/src/components/UI/Pages/VendorAppForm.tsx
@@ -1,22 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Form, Container, Button } from "semantic-ui-react";
 import Buttons from "../Atoms/Button/Buttons";
 import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
-import styles from "./signup.module.css";
+import { Dropdown } from 'semantic-ui-react'
+import styles from './vendorappform.module.css'
 
 export default function VendorAppForm(): React.ReactElement {
   const storeUserInfo = () => {};
 
+  const timeOptionsFromValues = [ //options for business hours starting from...
+    '8:30 AM',
+    '9:00 AM',
+    '9:30 AM',
+    '10:00 AM',
+    '10:30 AM',
+    '11:00 AM',
+    '11:30 AM',
+    '12:00 AM',    
+  ]
+
+  const timeOptionsToValues = [ //options for business hours starting to..
+    '8:30 PM',
+    '9:00 PM',
+    '9:30 PM',
+    '10:00 PM',
+    '10:30 PM',
+    '11:00 PM',
+    '11:30 PM',
+    '12:00 PM',
+  ]
+
+  const startTimeOptions = timeOptionsFromValues.map((element, key) => {
+    return {
+      key: timeOptionsFromValues[key],
+      text: element,
+      value: element
+    }
+  })
+
+  const endTimeOptions = timeOptionsToValues.map((element, key) => {
+    return {
+      key: timeOptionsToValues[key],
+      text: element,
+      value: element
+    }
+  })
+
   return (
     <Container className={styles.signUpWrapper}>
       <HeaderBar signUp={false} />
-      <h1>Sign Up Form (user account)</h1>
+      <h1>Sign Up Form (Vendor account)</h1>
       <Form className={styles.form}>
         <Form.Input
           // error={{ content: 'Please enter your first name', pointing: 'below' }}
           fluid
-          label="Address"
-          placeholder="Address"
+          label="Name"
+          placeholder="Name"
           id="form-input-first-name"
           required
           width={5}
@@ -24,10 +63,32 @@ export default function VendorAppForm(): React.ReactElement {
         <Form.Input
           // error='Please enter your last name'
           fluid
-          label=""
-          placeholder=""
+          label="Business Address"
+          placeholder="Business Address"
           required
           width={5}
+        />
+        <Form.Input
+          // error='Please enter your last name'
+          fluid
+          label="Website URL"
+          placeholder="Website URL"
+          required
+          width={5}
+        />
+        <h5>Business Hours: </h5>
+        <Dropdown
+          placeholder="From"
+          search
+          selection
+          options={startTimeOptions}
+        />
+        <Dropdown
+          placeholder="To"
+          search
+          selection
+          options={endTimeOptions}
+          className={styles.dropdown}
         />
         <Form.Input
           // error='Please enter your last name'
@@ -36,23 +97,14 @@ export default function VendorAppForm(): React.ReactElement {
           placeholder="Phone"
           required
           width={5}
+          className={styles.phoneInput}
         />
-
-        <Form.Input
-          // error='Please enter your last name'
-          fluid
-          label="Email Address"
-          placeholder="Email Address"
-          required
-          width={5}
-        />
+        <h5>Logo Upload</h5>
+        <input type="file" className={styles.fileInput} name="myfile" />
         <Form.Checkbox
           label="I agree to the Terms and Conditions"
-          // error={{
-          //   content: 'You must agree to the terms and conditions',
-          //   pointing: 'left',
-          // }}
           required
+          className={styles.checkBox}
         />
       </Form>
       <Container className={styles.btnContainer}>
