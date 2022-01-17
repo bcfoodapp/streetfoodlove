9d82eab83cd2b784dd74478110e67d0b8c897ea9
czhou578 <colizu2020@gmail.com>
2022-01-17 14:16:53 -0800

    put the input inside label

--- app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
+++ app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
@@ -13,24 +13,24 @@ export const FinalStarRating = (props: Props) => {
 
   return (
     <Container className={styles.rate}>
-      <input type="radio" id="star5" name="rate" value="5" disabled />
       <label htmlFor="star5" title="text">
+        <input type="radio" id="star5" name="rate" value="5" disabled />
         5 stars
       </label>
-      <input type="radio" id="star4" name="rate" value="4" disabled />
       <label htmlFor="star4" title="text">
+        <input type="radio" id="star4" name="rate" value="4" disabled />
         4 stars
       </label>
-      <input type="radio" id="star3" name="rate" value="3" checked />
       <label htmlFor="star3" title="text" style={stylesTest}>
+        <input type="radio" id="star3" name="rate" value="3" checked />
         3 stars
       </label>
-      <input type="radio" id="star2" name="rate" value="2" checked />
       <label htmlFor="star2" title="text" style={stylesTest}>
+        <input type="radio" id="star2" name="rate" value="2" checked />
         2 stars
       </label>
-      <input type="radio" id="star1" name="rate" value="1" checked />
       <label htmlFor="star1" title="text" style={stylesTest}>
+        <input type="radio" id="star1" name="rate" value="1" checked />
         1 star
       </label>
     </Container>
