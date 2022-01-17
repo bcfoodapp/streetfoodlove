17cd5f25e1e835e860edf2d4d6f161e1bb6610ae
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2022-01-17 14:35:21 -0800

    Edited Star Rating  (#84)
    * Merge branch 'main' into colin, fixed tsconfig error
    
    * deleted babel core type from package.json (added while debugging tsconfig error)
    
    * deleted babel core
    
    * created button, and ts declaration file
    
    * created popupinfo and buttons components
    
    * created searchbarinput component
    
    * did npm i, current node version is 8.1.3
    
    * added comment to buttons file
    
    * created vendor stylesheet and all needed components
    
    * added text area to vendor and event handlers
    
    * styled info cards
    
    * finished basic navbar layout
    
    * installed semantic-ui-css stylesheets, webpack issue needs fixing
    
    * login routed to, styled vendor page, created components for review
    
    * created review component, check out import in reviewlabel.tsx
    
    * link semantic-ui css
    
    * commit
    
    * fixed styling due to imported stylesheet
    
    * formatted review component
    
    * finished login page skeleton, npx prettier performed
    
    * conditional rendering review form, npx pretty, passing handlers to change state
    
    * added comments
    
    * added comments, deleted unwanted files
    
    * editing star rating
    
    * typed the props for buttons
    
    * fixed merge conflicts
    
    * trying to get reviews to show
    
    * commit
    
    * created landingpage for index url
    
    * commit
    
    * npx prettier and localstorage is updating
    
    * remove empty try-catch
    
    I don't know why I ignored errors in this code, but it looks like a terrible idea.
    
    * commit
    
    * npx prettier
    
    * finished signup page for user acct
    
    * npx prettier
    
    * finished account selection page
    
    * vendorappform design done
    
    * npx prettier
    
    * edited star rating
    
    * npx prettier
    
    * put the input inside label
    
    * npx pretty
    
    Co-authored-by: Makoto <makoto@emurasoft.com>

--- app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
+++ app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
@@ -1,28 +1,32 @@
 import { Container } from "semantic-ui-react";
 import styles from "./finalStarRating.module.css";
 
-export const FinalStarRating = () => {
+// stars will be the number of stars given that is retrieved from backend
+interface Props {
+  stars: number;
+}
+
+export const FinalStarRating = (props: Props) => {
+  const stylesTest = {
+    color: "#c59b08",
+  };
+
   return (
     <Container className={styles.rate}>
-      <input type="radio" id="star5" name="rate" value="5" disabled />
       <label htmlFor="star5" title="text">
-        5 stars
+        <input type="radio" id="star5" name="rate" value="5" disabled />5 stars
       </label>
-      <input type="radio" id="star4" name="rate" value="4" disabled />
       <label htmlFor="star4" title="text">
-        4 stars
+        <input type="radio" id="star4" name="rate" value="4" disabled />4 stars
       </label>
-      <input type="radio" id="star3" name="rate" value="3" checked />
-      <label htmlFor="star3" title="text">
-        3 stars
+      <label htmlFor="star3" title="text" style={stylesTest}>
+        <input type="radio" id="star3" name="rate" value="3" checked />3 stars
       </label>
-      <input type="radio" id="star2" name="rate" value="2" checked />
-      <label htmlFor="star2" title="text">
-        2 stars
+      <label htmlFor="star2" title="text" style={stylesTest}>
+        <input type="radio" id="star2" name="rate" value="2" checked />2 stars
       </label>
-      <input type="radio" id="star1" name="rate" value="1" checked />
-      <label htmlFor="star1" title="text">
-        1 star
+      <label htmlFor="star1" title="text" style={stylesTest}>
+        <input type="radio" id="star1" name="rate" value="1" checked />1 star
       </label>
     </Container>
   );
