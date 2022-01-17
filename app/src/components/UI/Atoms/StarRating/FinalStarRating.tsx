099bd07a98a914fd5260e84d367058ad2770be52
czhou578 <colizu2020@gmail.com>
2022-01-17 14:18:37 -0800

    npx pretty

--- app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
+++ app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
@@ -14,24 +14,19 @@ export const FinalStarRating = (props: Props) => {
   return (
     <Container className={styles.rate}>
       <label htmlFor="star5" title="text">
-        <input type="radio" id="star5" name="rate" value="5" disabled />
-        5 stars
+        <input type="radio" id="star5" name="rate" value="5" disabled />5 stars
       </label>
       <label htmlFor="star4" title="text">
-        <input type="radio" id="star4" name="rate" value="4" disabled />
-        4 stars
+        <input type="radio" id="star4" name="rate" value="4" disabled />4 stars
       </label>
       <label htmlFor="star3" title="text" style={stylesTest}>
-        <input type="radio" id="star3" name="rate" value="3" checked />
-        3 stars
+        <input type="radio" id="star3" name="rate" value="3" checked />3 stars
       </label>
       <label htmlFor="star2" title="text" style={stylesTest}>
-        <input type="radio" id="star2" name="rate" value="2" checked />
-        2 stars
+        <input type="radio" id="star2" name="rate" value="2" checked />2 stars
       </label>
       <label htmlFor="star1" title="text" style={stylesTest}>
-        <input type="radio" id="star1" name="rate" value="1" checked />
-        1 star
+        <input type="radio" id="star1" name="rate" value="1" checked />1 star
       </label>
     </Container>
   );
