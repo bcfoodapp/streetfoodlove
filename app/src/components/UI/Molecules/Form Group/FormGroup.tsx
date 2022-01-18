adc13a71c70e3a531faf67a6675e4cc6a75c4987
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2022-01-18 10:38:31 -0800

    Finished edit account profile UI  (#89)
    * Merge branch 'main' into colin, fixed tsconfig error
    
    * deleted babel core type from package.json (added while debugging tsconfig error)
    
    * deleted babel core
    
    * created button, and ts declaration file
    
    * created popupinfo and buttons components
    
    * created searchbarinput component
    
    * did npm i, current node version is 8.1.3
    
    * added comment to buttons file
    
    * created vendor stylesheet and all needed components
    
    * added text area to vendor and event handlers
    
    * styled info cards
    
    * finished basic navbar layout
    
    * installed semantic-ui-css stylesheets, webpack issue needs fixing
    
    * login routed to, styled vendor page, created components for review
    
    * created review component, check out import in reviewlabel.tsx
    
    * link semantic-ui css
    
    * commit
    
    * fixed styling due to imported stylesheet
    
    * formatted review component
    
    * finished login page skeleton, npx prettier performed
    
    * conditional rendering review form, npx pretty, passing handlers to change state
    
    * added comments
    
    * added comments, deleted unwanted files
    
    * editing star rating
    
    * typed the props for buttons
    
    * fixed merge conflicts
    
    * trying to get reviews to show
    
    * commit
    
    * created landingpage for index url
    
    * commit
    
    * npx prettier and localstorage is updating
    
    * remove empty try-catch
    
    I don't know why I ignored errors in this code, but it looks like a terrible idea.
    
    * commit
    
    * npx prettier
    
    * finished signup page for user acct
    
    * npx prettier
    
    * finished account selection page
    
    * vendorappform design done
    
    * npx prettier
    
    * edited star rating
    
    * npx prettier
    
    * put the input inside label
    
    * npx pretty
    
    * made basic layout of account profile
    
    * added a border
    
    * npx prettier
    
    * added save button
    
    * npx prettier
    
    * finished account editing ui
    
    Co-authored-by: Makoto <makoto@emurasoft.com>

--- app/src/components/UI/Molecules/Form Group/FormGroup.tsx
+++ app/src/components/UI/Molecules/Form Group/FormGroup.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Container, Form } from "semantic-ui-react";
+import { Container, Form, Icon } from "semantic-ui-react";
 
-const FormGroup: React.FC = () => {
+const FormGroup: React.FC<{ disabled: boolean }> = ({ disabled }) => {
   return (
     <Container>
       <Form>
         <Form.Group unstackable widths={2}>
-          <Form.Input label="First name" placeholder="First name" />
-          <Form.Input label="Last name" placeholder="Last name" />
+          <Form.Input
+            label="First name"
+            placeholder="First name"
+            disabled={disabled}
+          />
+          <Form.Input
+            label="Last name"
+            placeholder="Last name"
+            disabled={disabled}
+          />
         </Form.Group>
         <Form.Group widths={2}>
-          <Form.Input label="Address" placeholder="Address" />
-          <Form.Input label="Phone" placeholder="Phone" />
+          <Form.Input
+            label="Address"
+            placeholder="Address"
+            disabled={disabled}
+          />
+          <Form.Input label="Phone" placeholder="Phone" disabled={disabled} />
         </Form.Group>
       </Form>
     </Container>
