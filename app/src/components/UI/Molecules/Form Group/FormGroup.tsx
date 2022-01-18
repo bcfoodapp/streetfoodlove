9bc5e01fde5d0717c208e17244716acb34af5f07
czhou578 <colizu2020@gmail.com>
2022-01-17 16:59:31 -0800

    npx prettier

--- app/src/components/UI/Molecules/Form Group/FormGroup.tsx
+++ app/src/components/UI/Molecules/Form Group/FormGroup.tsx
@@ -6,16 +6,16 @@ const FormGroup: React.FC = () => {
     <Container>
       <Form>
         <Form.Group unstackable widths={2}>
-          <Form.Input label='First name' placeholder='First name'/>
-          <Form.Input label='Last name' placeholder='Last name'/>
+          <Form.Input label="First name" placeholder="First name" />
+          <Form.Input label="Last name" placeholder="Last name" />
         </Form.Group>
         <Form.Group widths={2}>
-          <Form.Input label='Address' placeholder='Address'/>
-          <Form.Input label='Phone' placeholder='Phone'/>
+          <Form.Input label="Address" placeholder="Address" />
+          <Form.Input label="Phone" placeholder="Phone" />
         </Form.Group>
       </Form>
     </Container>
-  )
-}
+  );
+};
 
-export default FormGroup
+export default FormGroup;
--- app/src/components/UI/Pages/AccountProfile.tsx
+++ app/src/components/UI/Pages/AccountProfile.tsx
@@ -1,27 +1,42 @@
 import React from "react";
-import { Container} from "semantic-ui-react";
-import styles from './accountprofile.module.css'
-import { Tab } from 'semantic-ui-react'
+import { Container } from "semantic-ui-react";
+import styles from "./accountprofile.module.css";
+import { Tab } from "semantic-ui-react";
 import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
 import FormGroup from "../Molecules/Form Group/FormGroup";
 
 const AccountProfile: React.FC = () => {
   const panes = [
-    { menuItem: 'Account Settings', render: () => <Tab.Pane className={styles.pane}><FormGroup /></Tab.Pane> },
-    { menuItem: 'Personal Information', render: () => <Tab.Pane className={styles.pane}>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Privacy', render: () => <Tab.Pane className={styles.pane}>Tab 3 Content</Tab.Pane> },
-  ]
-
+    {
+      menuItem: "Account Settings",
+      render: () => (
+        <Tab.Pane className={styles.pane}>
+          <FormGroup />
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "Personal Information",
+      render: () => <Tab.Pane className={styles.pane}>Tab 2 Content</Tab.Pane>,
+    },
+    {
+      menuItem: "Privacy",
+      render: () => <Tab.Pane className={styles.pane}>Tab 3 Content</Tab.Pane>,
+    },
+  ];
 
   return (
     <Container className={styles.wrapper}>
       <HeaderBar />
       <h2>Profile</h2>
       <Container className={styles.profileActions}>
-        <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes}/>
+        <Tab
+          menu={{ fluid: true, vertical: true, tabular: true }}
+          panes={panes}
+        />
       </Container>
     </Container>
-  )
-}
+  );
+};
 
-export default AccountProfile
+export default AccountProfile;
