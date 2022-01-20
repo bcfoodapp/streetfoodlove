352df06de70e1df4a11422eca28d25b9d4273558
czhou578 <colizu2020@gmail.com>
2022-01-20 10:53:59 -0800

    finished basic layout of youraccountuser

---
+++ app/src/components/UI/Molecules/YourAccount/YourAccountOption.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from "react";
+import { Container, Icon, Header } from "semantic-ui-react";
+import styles from './youracctbox.module.css'
+
+const YourAccountOption: FC<{ header: string; iconName: string }> = ({
+  header,
+  iconName,
+}) => {
+  return (
+    <Container className={styles.wrapper}>
+      <Header as={"h1"} className={styles.header}>{header}</Header>
+      <Container className={styles.iconWrap}>
+        {iconName! === "favorite" ? (
+          <Icon name={"favorite"} size="large" />
+        ) : iconName! === "history" ? (
+          <Icon name={"history"} size="large" />
+        ) : null}
+      </Container>
+    </Container>
+  );
+};
+
+export default YourAccountOption;
---
+++ app/src/components/UI/Pages/YourAccountUser.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
+import YourAccountOption from "../Molecules/YourAccount/YourAccountOption";
+import styles from './youracctuser.module.css'
+
+const YourAccountUser: React.FC = () => {
+  return (
+    <Container className={styles.wrapper}>
+      <HeaderBar />
+      <Container className={styles.content}>
+        <YourAccountOption header="Your Favorites" iconName="favorite" />
+        <YourAccountOption header="Search History" iconName="history" />
+        <YourAccountOption header="Search History" iconName="history" />
+        {/* <YourAccountOption header="Search History" iconName="history" /> */}
+      </Container>
+    </Container>
+  )
+}
+
+export default YourAccountUser
