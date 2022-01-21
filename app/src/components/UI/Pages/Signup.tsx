94f599f2737ec39f41e91071b903f2dc832a2d5f
Makoto <makoto@emurasoft.com>
2022-01-20 21:16:50 -0800

    move submit handler

--- app/src/components/UI/Pages/Signup.tsx
+++ app/src/components/UI/Pages/Signup.tsx
@@ -19,7 +19,22 @@ export default function Signup(): React.ReactElement {
     <Container className={styles.signUpWrapper}>
       <HeaderBar signUp={false} />
       <h1>Sign Up Form (user account)</h1>
-      <Form className={styles.form}>
+      <Form
+        className={styles.form}
+        onSubmit={async () => {
+          await createUser({
+            ID: uuid(),
+            Username: username,
+            Photo: uuid(),
+            UserType: UserType.Customer,
+            Email: email,
+            FirstName: firstName,
+            LastName: lastName,
+            Password: password,
+          });
+          alert("created account");
+        }}
+      >
         <Form.Input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
@@ -80,28 +95,12 @@ export default function Signup(): React.ReactElement {
           // }}
           required
         />
+        <Container className={styles.btnContainer}>
+          <Buttons color="green" signup>
+            Sign Up
+          </Buttons>
+        </Container>
       </Form>
-      <Container className={styles.btnContainer}>
-        <Buttons
-          clicked={() => {
-            createUser({
-              ID: uuid(),
-              Username: username,
-              Photo: uuid(),
-              UserType: UserType.Customer,
-              Email: email,
-              FirstName: firstName,
-              LastName: lastName,
-              Password: password,
-            });
-            alert("created account");
-          }}
-          color="green"
-          signup
-        >
-          Sign Up
-        </Buttons>
-      </Container>
     </Container>
   );
 }
