b2447be647e0f0927250f9d03357fcf7adb8b114
Makoto <makoto@emurasoft.com>
2022-01-21 18:11:22 -0800

    set parameter of setStarRating as StarRatingInteger

--- app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
+++ app/src/components/UI/Atoms/StarRating/FinalStarRating.tsx
@@ -1,6 +1,6 @@
 import { Container } from "semantic-ui-react";
 import styles from "./starRating.module.css";
-import React, { RefObject, useCallback, useEffect } from "react";
+import React from "react";
 import { Rating } from "semantic-ui-react";
 
 // stars will be the number of stars given that is retrieved from backend
@@ -8,7 +8,7 @@ interface Props {
   starRating: number;
 }
 
-export const FinalStarRating = ({ starRating = 3 }: Props) => {
+export const FinalStarRating = ({ starRating }: Props) => {
   return (
     <Container className={styles.rate}>
       <Rating
--- app/src/components/UI/Atoms/StarRating/StarRating.tsx
+++ app/src/components/UI/Atoms/StarRating/StarRating.tsx
@@ -1,29 +1,44 @@
 import { Container, RatingProps } from "semantic-ui-react";
 import styles from "./starRating.module.css";
-import { ChangeEvent } from "react";
 import { StarRatingInteger } from "../../../../api";
 import { Rating } from "semantic-ui-react";
 
 interface Props {
+  // starRating is null if not set
   starRating: StarRatingInteger | null;
-  setStarRating: (rating: RatingProps) => void;
+  setStarRating: (rating: StarRatingInteger) => void;
 }
 
 /**
  * This element represents the star rating that appears in the review form
  */
 export const StarRating = ({ starRating, setStarRating }: Props) => {
-  const trackStarCount = (e: any, rating: RatingProps) => {
-    setStarRating(rating);
+  const trackStarCount = (_: any, rating: RatingProps) => {
+    if (
+      !(
+        rating.rating &&
+        Number.isInteger(rating.rating) &&
+        1 <= rating.rating &&
+        rating.rating <= 5
+      )
+    ) {
+      throw new Error(`unexpected rating value: ${rating.rating}`);
+    }
+    setStarRating(rating.rating as StarRatingInteger);
   };
+
+  let value = 0;
+  if (starRating !== null) {
+    value = starRating;
+  }
 
   return (
     <Container className={styles.starRatingCtn}>
       <Rating
         maxRating={5}
-        defaultRating={0}
         icon="star"
         size="huge"
+        rating={value}
         onRate={trackStarCount}
       />
     </Container>
