a07f640755d8759fe4a410aa4f13bf1ee93510d7
czhou578 <colizu2020@gmail.com>
2022-01-22 18:13:29 -0800

    added two new props to formgroup, npx pretty

--- app/src/components/UI/Molecules/Form Group/FormGroup.tsx
+++ app/src/components/UI/Molecules/Form Group/FormGroup.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Container, Form, Icon } from "semantic-ui-react";
+import { UserProtected } from "../../../../api";
 
-const FormGroup: React.FC<{ disabled: boolean; formLabels: string[] }> = ({
-  disabled,
-  formLabels,
-}) => {
+const FormGroup: React.FC<{
+  disabled: boolean;
+  formLabels: string[];
+  value: UserProtected;
+  onChange: (value: UserProtected) => void;
+}> = ({ disabled, formLabels, value, onChange }) => {
   var tempElement: any;
   return (
     <Container>
