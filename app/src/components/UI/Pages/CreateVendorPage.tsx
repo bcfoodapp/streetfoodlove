0682abc0116cfdfac456b9734bfa60f81c83675d
czhou578 <colizu2020@gmail.com>
2022-01-25 14:05:30 -0800

    edited fields

--- app/src/components/UI/Pages/CreateVendorPage.tsx
+++ app/src/components/UI/Pages/CreateVendorPage.tsx
@@ -1,28 +1,65 @@
-import { Container, Form, Header } from "semantic-ui-react"
+import { Button, Container, Form, Header, Input, Select, TextArea } from "semantic-ui-react"
 import HeaderBar from "../Molecules/HeaderBar/HeaderBar"
 import styles from './createvendorpage.module.css'
+
+const genderOptions = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+  { key: 'o', text: 'Other', value: 'other' },
+]
+
 
 const CreateVendorPage: React.FC = () => {
   return (
     <Container className={styles.wrapper}>
       <HeaderBar logout />
-      <Header as={"h2"}>Create New Vendor Page</Header>
-      <Form>
-        <Form.Input 
-          fluid
-          required
-        />
-        <Form.Input 
-          fluid
-          required
-          
-        />
-        <Form.Input 
-          fluid
-          required
-          
-        />
-      </Form>
+      <Header as={"h2"} className={styles.header}>Create New Vendor Page</Header>
+      <Form className={styles.form}>
+    <Form.Group widths='equal'>
+      <Form.Field
+        id='form-input-control-first-name'
+        control={Input}
+        label='First name'
+        placeholder='First name'
+      />
+      <Form.Field
+        id='form-input-control-last-name'
+        control={Input}
+        label='Last name'
+        placeholder='Last name'
+      />
+      <Form.Field
+        control={Select}
+        options={genderOptions}
+        label={{ children: 'Gender', htmlFor: 'form-select-control-gender' }}
+        placeholder='Gender'
+        search
+        searchInput={{ id: 'form-select-control-gender' }}
+      />
+    </Form.Group>
+    <Form.Field
+      id='form-textarea-control-opinion'
+      control={TextArea}
+      label='Food Truck Description'
+      placeholder='Food Truck Description'
+    />
+    <Form.Field
+      id='form-input-control-error-email'
+      control={Input}
+      label='Email'
+      placeholder='joe@schmoe.com'
+      error={{
+        content: 'Please enter a valid email address',
+        pointing: 'below',
+      }}
+    />
+    <Form.Field
+      id='form-button-control-public'
+      control={Button}
+      content='Confirm'
+      label='Label with htmlFor'
+    />
+  </Form>
     </Container>
   )
 }
