04999cc9ba453959485c9833703f1c331879c000
czhou578 <colizu2020@gmail.com>
2022-01-25 16:24:22 -0800

    npx prettier, finished basic vendor dashboard ui

--- app/src/components/UI/Pages/VendorDashboard.tsx
+++ app/src/components/UI/Pages/VendorDashboard.tsx
@@ -1,26 +1,64 @@
-import { Container, Card, Icon } from "semantic-ui-react"
+import { Container, Card, Icon, Header } from "semantic-ui-react";
+import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
+import styles from "./vendordashboard.module.css";
 
 const VendorDashBoard: React.FC = () => {
   return (
-    <Container>
-      <Card>
-        {/* <Image src='/images/avatar/large/daniel.jpg' wrapped ui={false} /> */}
-        <Card.Content>
-          <Card.Header>Daniel</Card.Header>
-          <Card.Meta>Joined in 2016</Card.Meta>
-          <Card.Description>
-            Daniel is a comedian living in Nashville.
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name='user' />
-            10 Friends
-          </a>
-        </Card.Content>
-      </Card>
+    <Container className={styles.wrapper}>
+      <HeaderBar logout />
+      <Header as={"h2"} className={styles.header}>
+        Vendor Dashboard
+      </Header>
+      <Card.Group className={styles.cardGroup}>
+        <Card className={styles.card}>
+          <Icon name="setting" size="huge" className={styles.icon} />
+          <Card.Content className={styles.content}>
+            <Card.Header>Customize Vendor Page</Card.Header>
+            <Card.Meta>Updated in 2016</Card.Meta>
+            <Card.Description>
+              Customize existing vendor pages!
+            </Card.Description>
+          </Card.Content>
+          {/* <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              10 Friends
+            </a>
+          </Card.Content> */}
+        </Card>
+        <Card className={styles.card}>
+          <Icon name="line graph" size="huge" className={styles.icon} />
+          <Card.Content className={styles.content}>
+            <Card.Header>Trends</Card.Header>
+            <Card.Meta>Updated in 2016</Card.Meta>
+            <Card.Description>
+              Analyze your business performance!
+            </Card.Description>
+          </Card.Content>
+          {/* <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              10 Friends
+            </a>
+          </Card.Content> */}
+        </Card>
+        <Card className={styles.card}>
+          <Icon name="write" size="huge" className={styles.icon} />
+          <Card.Content className={styles.content}>
+            <Card.Header>New Reviews!</Card.Header>
+            <Card.Meta>Updated in 2016</Card.Meta>
+            <Card.Description>Look at recent reviews!</Card.Description>
+          </Card.Content>
+          {/* <Card.Content extra>
+            <a>
+              <Icon name='user' />
+              10 Friends
+            </a>
+          </Card.Content> */}
+        </Card>
+      </Card.Group>
     </Container>
-  )
-}
+  );
+};
 
-export default VendorDashBoard
+export default VendorDashBoard;
