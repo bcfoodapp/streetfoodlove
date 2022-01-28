ac65ab12406ec78419a9a8d6f50bd223a83fa44b
czhou578 <colizu2020@gmail.com>
2022-01-27 21:08:03 -0800

    banner displaying and closing properly

--- app/src/components/UI/Atoms/Message/MessageError.tsx
+++ app/src/components/UI/Atoms/Message/MessageError.tsx
@@ -1,14 +1,26 @@
-import React from "react";
-import { Message } from "semantic-ui-react";
-import styles from './msgerror.module.css'
+import React, { useState, MouseEvent } from "react";
+import { Container, Message, MessageProps } from "semantic-ui-react";
+import styles from "./msgerror.module.css";
 
-const MessageError: React.FC<{errorMsg: string}> = ({errorMsg}) => (
-  <Message negative className={styles.msg}>
-    <Message.Header>Error</Message.Header>
-    <Message.List>
-      <Message.Item>{errorMsg}</Message.Item>
-    </Message.List>
-  </Message>
-);
+const MessageError: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
+  const [visibleMsg, setVisibleMsg] = useState<boolean>(true);
+
+  const dismissHandler = (e: MouseEvent<HTMLElement>, data: MessageProps) => {
+    setVisibleMsg(false);
+  };
+
+  return (
+    <Container className={styles.wrapper}>
+      {visibleMsg ? (
+        <Message negative className={styles.msg} onDismiss={dismissHandler}>
+          <Message.Header>Error</Message.Header>
+          <Message.List>
+            <Message.Item>{errorMsg}</Message.Item>
+          </Message.List>
+        </Message>
+      ) : null}
+    </Container>
+  );
+};
 
 export default MessageError;
