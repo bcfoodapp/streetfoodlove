5dbf6fe3c1fc56234ce2f5b19dc48fb716735fae
czhou578 <colizu2020@gmail.com>
2022-01-28 21:18:55 -0800

    finished vendorappform validation except for checkbox

--- app/src/components/UI/Pages/VendorAppForm.tsx
+++ app/src/components/UI/Pages/VendorAppForm.tsx
@@ -4,6 +4,17 @@ import Buttons from "../Atoms/Button/Buttons";
 import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
 import { Dropdown } from "semantic-ui-react";
 import styles from "./vendorappform.module.css";
+import { Formik, FormikProps, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+interface inputValues {
+  name: string;
+  businessAddress: string;
+  phoneNumber: string;
+  fromHour: string;
+  toHour: string;
+  website: string;
+}
 
 export default function VendorAppForm(): React.ReactElement {
   const storeUserInfo = () => {};
@@ -48,72 +59,156 @@ export default function VendorAppForm(): React.ReactElement {
     };
   });
 
+  const initialValues: inputValues = {
+    name: "",
+    businessAddress: "",
+    website: "",
+    fromHour: "",
+    toHour: "",
+    phoneNumber: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    businessAddress: Yup.string().required("Required"),
+    website: Yup.string().required("Required"),
+    fromHour: Yup.string().required("Required"),
+    toHour: Yup.string().required("Required"),
+    phoneNumber: Yup.string().required("Required"),
+  });
+
   return (
     <Container className={styles.signUpWrapper}>
       <HeaderBar login />
       <h1>Sign Up Form (Vendor account)</h1>
-      <Form className={styles.form}>
-        <Form.Input
-          // error={{ content: 'Please enter your first name', pointing: 'below' }}
-          fluid
-          label="Name"
-          placeholder="Name"
-          id="form-input-first-name"
-          required
-          width={5}
-        />
-        <Form.Input
-          // error='Please enter your last name'
-          fluid
-          label="Business Address"
-          placeholder="Business Address"
-          required
-          width={5}
-        />
-        <Form.Input
-          // error='Please enter your last name'
-          fluid
-          label="Website URL"
-          placeholder="Website URL"
-          required
-          width={5}
-        />
-        <h5>Business Hours: </h5>
-        <Dropdown
-          placeholder="From"
-          search
-          selection
-          options={startTimeOptions}
-        />
-        <Dropdown
-          placeholder="To"
-          search
-          selection
-          options={endTimeOptions}
-          className={styles.dropdown}
-        />
-        <Form.Input
-          // error='Please enter your last name'
-          fluid
-          label="Phone"
-          placeholder="Phone"
-          required
-          width={5}
-          className={styles.phoneInput}
-        />
-        <h5>Logo Upload</h5>
-        <input type="file" className={styles.fileInput} name="myfile" />
-        <Form.Checkbox
-          label="I agree to the Terms and Conditions"
-          required
-          className={styles.checkBox}
-        />
-      </Form>
-      <Container className={styles.btnContainer}>
-        <Buttons clicked={() => storeUserInfo} color="green" signup>
-          Sign Up
-        </Buttons>
-      </Container>
+
+      <Formik
+        enableReinitialize
+        onSubmit={storeUserInfo}
+        validateOnChange={true}
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+      >
+        {(formProps: FormikProps<inputValues>) => {
+          const {
+            dirty,
+            isValid,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            touched,
+            errors,
+            values,
+            handleReset,
+            setFieldValue,
+          } = formProps;
+
+          return (
+            <Form
+              className={styles.form}
+              onSubmit={handleSubmit}
+              onReset={handleReset}
+            >
+              <Form.Input
+                // error={{ content: 'Please enter your first name', pointing: 'below' }}
+                fluid
+                label="Name"
+                placeholder="Name"
+                name={"name"}
+                required
+                width={5}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+                error={touched.name && Boolean(errors.name)}
+              />
+              <Form.Input
+                // error='Please enter your last name'
+                fluid
+                label="Business Address"
+                placeholder="Business Address"
+                required
+                width={5}
+                name={"businessAddress"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.businessAddress}
+                error={
+                  touched.businessAddress && Boolean(errors.businessAddress)
+                }
+              />
+              <Form.Input
+                // error='Please enter your last name'
+                fluid
+                label="Website URL"
+                placeholder="Website URL"
+                required
+                width={5}
+                name={"website"}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.website}
+                error={touched.website && Boolean(errors.website)}
+              />
+              <h5>Business Hours: </h5>
+              <Dropdown
+                placeholder="From"
+                selection
+                options={startTimeOptions}
+                name={"fromHour"}
+                onBlur={handleBlur}
+                error={touched.fromHour && Boolean(errors.fromHour)}
+                value={values.fromHour}
+                required
+                onChange={(_, fromHour) => {
+                  setFieldValue("fromHour", fromHour.value);
+                }}
+              />
+              <Dropdown
+                placeholder="To"
+                search
+                selection
+                options={endTimeOptions}
+                className={styles.dropdown}
+                name={"toHour"}
+                onBlur={handleBlur}
+                error={touched.toHour && Boolean(errors.toHour)}
+                value={values.toHour}
+                required
+                onChange={(_, toHour) => {
+                  setFieldValue("toHour", toHour.value);
+                }}
+              />
+              <Form.Input
+                // error='Please enter your last name'
+                fluid
+                label="Phone"
+                placeholder="Phone"
+                name={"phoneNumber"}
+                required
+                width={5}
+                className={styles.phoneInput}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.phoneNumber}
+                error={touched.phoneNumber && Boolean(errors.phoneNumber)}
+              />
+              <h5>Logo Upload</h5>
+              <input type="file" className={styles.fileInput} name="myfile" />
+              <Form.Checkbox
+                label="I agree to the Terms and Conditions"
+                required
+                className={styles.checkBox}
+              />
+              <Container className={styles.btnContainer}>
+                <Buttons color="green" signup dirty={dirty} valid={isValid}>
+                  Sign Up
+                </Buttons>
+              </Container>
+            </Form>
+          );
+        }}
+      </Formik>
     </Container>
   );
 }
