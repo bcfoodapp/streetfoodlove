88c2258fbd0f7d6d25c40e2cb8a5f3cfc38157a9
Makoto <36318069+MakotoE@users.noreply.github.com>
2022-02-01 13:15:25 -0800

    Make log out button functional (#155)

--- app/src/components/UI/Molecules/HeaderBar/HeaderBar.tsx
+++ app/src/components/UI/Molecules/HeaderBar/HeaderBar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Icon, Menu, Dropdown, Button } from "semantic-ui-react";
+import { Icon, Menu, Dropdown, DropdownItemProps } from "semantic-ui-react";
 import Buttons from "../../Atoms/Button/Buttons";
 import { SearchBox } from "../../Atoms/SearchBox/SearchBox";
 import styles from "./headerbar.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { deleteCredentials } from "../../../../api";
 
 /**
  * Returns the headerbar element
@@ -16,24 +17,32 @@ const ProfileIcon = (
   </span>
 );
 
-const options = [
-  {
-    key: "user",
-    text: (
-      <span>
-        Signed in as <strong>Colin Zhou</strong>
-      </span>
-    ),
-    disabled: true,
-  },
-  { key: "profile", text: "Profile Settings" },
-  { key: "page", text: "Create Vendor Page" },
-  { key: "help", text: "Help" },
-  { key: "sign-out", text: "Log Out" },
-];
-
 export default function HeaderBar(): React.ReactElement {
   const token = useSelector((state: any) => state.token.token);
+  const navigate = useNavigate();
+
+  const options: DropdownItemProps[] = [
+    {
+      key: "user",
+      text: (
+        <span>
+          Signed in as <strong>Colin Zhou</strong>
+        </span>
+      ),
+      disabled: true,
+    },
+    { key: "profile", text: "Profile Settings" },
+    { key: "page", text: "Create Vendor Page" },
+    { key: "help", text: "Help" },
+    {
+      key: "log-out",
+      text: "Log Out",
+      onClick: () => {
+        deleteCredentials();
+        navigate("/");
+      },
+    },
+  ];
 
   return (
     <Menu size="massive">
