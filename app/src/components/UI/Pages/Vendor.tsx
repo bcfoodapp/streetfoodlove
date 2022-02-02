f3b534f089bcd27fa46ce00ab1faa14a4f791530
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2022-02-02 09:43:55 -0800

    Pop up vendor link (#167)
    * commit
    
    * popup title now links to vendor page

--- app/src/components/UI/Pages/Vendor.tsx
+++ app/src/components/UI/Pages/Vendor.tsx
@@ -19,7 +19,6 @@ import { v4 as uuid } from "uuid";
 import { RootState, useAppSelector } from "../../../store";
 import { DateTime } from "luxon";
 import jwtDecode from "jwt-decode";
-import MessageError from "../Atoms/Message/MessageError";
 
 /**
  * Displays the vendor page of a vendor, including listed reviews and add review button
@@ -71,6 +70,8 @@ export function Vendor(): React.ReactElement {
     // Add current user to users list
     usersMultipleTrigger([...reviewsQuery.data!.map((r) => r.UserID), userID]);
   };
+
+  console.log(reviewsQuery.data);
 
   return (
     <>
