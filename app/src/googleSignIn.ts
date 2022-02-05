a7941b886fe1db9441bf9b20eb46b99f8e46f24c
Makoto <makoto@emurasoft.com>
2022-02-05 13:51:06 -0800

    decode token

--- app/src/googleSignIn.ts
+++ app/src/googleSignIn.ts
@@ -1,5 +1,13 @@
 // Logs in with Google account, creating an account if necessary. On success, token in store is set. Given token is the
 // one that Google provides.
+import jwtDecode from "jwt-decode";
+
+interface GoogleClaims {
+  family_name: string;
+  given_name: string;
+  email: string;
+}
+
 export function signInWithGoogle(token: string) {
-  console.log("token:", token);
+  console.log("token:", jwtDecode<GoogleClaims>(token));
 }
