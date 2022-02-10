4bba4c5ab6d5c552b7ff311424771a866e7d2c14
Makoto <makoto@emurasoft.com>
2022-02-09 17:43:52 -0800

    Display version

--- app/src/components/UI/Pages/LandingPage.tsx
+++ app/src/components/UI/Pages/LandingPage.tsx
@@ -1,11 +1,14 @@
 import { Container } from "semantic-ui-react";
 import Map from "../Atoms/Map";
-import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
 import styles from "./landingpage.module.css";
+import config from "../../../configuration.json";
 
 export const LandingPage = () => {
   return (
     <>
+      <Container textAlign="center">
+        <p>Version {config.version}</p>
+      </Container>
       <Container className={styles.mapWrap}>
         <Map />
       </Container>
