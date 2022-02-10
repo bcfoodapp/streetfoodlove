ceeed14f80df5b3dea3251000b27dec575169d03
czhou578 <colizu2020@gmail.com>
2022-02-09 18:54:21 -0800

    npx prettier

--- app/src/components/UI/Pages/LandingPage.tsx
+++ app/src/components/UI/Pages/LandingPage.tsx
@@ -1,9 +1,8 @@
 import { Container } from "semantic-ui-react";
 import Map from "../Atoms/Map";
-import styles from './landingpage.module.css'
+import styles from "./landingpage.module.css";
 
 export const LandingPage = () => {
-
   return (
     <>
       <Container>
