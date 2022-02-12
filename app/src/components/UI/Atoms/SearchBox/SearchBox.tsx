1ffbd817f40d3cbb02aa183a54f589b42eb05034
czhou578 <colizu2020@gmail.com>
2022-02-11 17:38:15 -0800

    modified searchbox

--- app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
+++ app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
@@ -1,26 +1,42 @@
-import React from "react";
-import { Container, Input, Menu } from "semantic-ui-react";
+import React, { ChangeEvent, useState } from "react";
+import { Container, Input, InputOnChangeData, Menu } from "semantic-ui-react";
 import styles from "./searchbox.module.css";
 import Buttons from "../Button/Buttons";
+import { Field } from "formik";
 
 /**
  * This is the searchbox for the header
  */
 
 export const SearchBox: React.FC = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const onChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    data: InputOnChangeData
+  ) => {
+    setSearchValue(data.value);
+  };
+
   const searchEnterHandler = () => {};
 
   return (
     <Menu.Item className={styles.searchBox}>
-      <Input
-        icon={
-          <Buttons enter color={"green"} clicked={searchEnterHandler}>
-            Enter
-          </Buttons>
-        }
-        placeholder="Search..."
-        focus
-      />
+      <Field>
+        {({ form: {dirty, valid } }) => (
+          <Input
+            icon={
+              <Buttons enter color={"green"} clicked={searchEnterHandler} dirty={dirty} valid={valid}>
+                Enter
+              </Buttons>
+            }
+            placeholder="Search..."
+            focus
+            onChange={onChange}
+            value={searchValue}
+          />
+        )}
+      </Field>
     </Menu.Item>
   );
 };
