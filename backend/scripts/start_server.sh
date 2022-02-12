c106cd63473cffa81e037fb4b4ae7eb6108acc5b
Makoto <makoto@emurasoft.com>
2022-02-11 21:06:35 -0800

    fix script

--- backend/scripts/start_server.sh
+++ backend/scripts/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Script to run the server in EC2 instance
 cd /home/ec2-user/backend
-go build .
-SECRETS_FILE=/home/ec2-user/secrets.json sudo -E ./streetfoodlove &> log &
+SECRETS_FILE=/home/ec2-user/secrets.json GOCACHE=/home/ec2-user/.cache/go-build sudo -E go run . &> log &
