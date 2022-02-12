5eeb5a99021886bf4d11df86a5d2211943dd0c2b
Makoto <makoto@emurasoft.com>
2022-02-12 11:41:12 -0800

    set version

--- backend/scripts/start_server.sh
+++ backend/scripts/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Script to run the server in EC2 instance
 cd /home/ec2-user/backend
+echo '"deploy test"' > version.json
+echo "\"${DEPLOYMENT_ID}\"" > version.json
+aws configure set region us-west-2
+aws deploy get-deployment --deployment-id $DEPLOYMENT_ID
+aws deploy get-deployment --deployment-id $DEPLOYMENT_ID --query 'revision.gitHubLocation.commitId'
 SECRETS_FILE=/home/ec2-user/secrets.json sudo -E go run . &> log &
