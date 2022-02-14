d49152fcc6c9ccf3821fe04e6288ff1a02c69301
czhou578 <colizu2020@gmail.com>
2022-02-14 14:52:29 -0800

    npx pretty, results showing

--- app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
+++ app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
@@ -1,13 +1,15 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, SetStateAction, useState } from "react";
 import {
   Container,
   Input,
   InputOnChangeData,
   Menu,
   Search,
+  SearchProps,
 } from "semantic-ui-react";
 import styles from "./searchbox.module.css";
 import Buttons from "../Button/Buttons";
+import { useVendorsQuery, Vendor } from "../../../../api";
 import { Field } from "formik";
 
 /**
@@ -25,27 +27,48 @@ const inputBox = (
     focus
     size={"small"}
     className={styles.inputBox}
-    // fluid
-    // onChange={onChange}
-    // value={searchValue}
   />
 );
 
 export const SearchBox: React.FC = () => {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchResult, setSearchResult] = useState<Vendor[]>([]);
+  const { data: vendorsList } = useVendorsQuery();
 
-  const onChange = (
-    e: ChangeEvent<HTMLInputElement>,
-    data: InputOnChangeData
+  const onSearchChange = (
+    event: React.MouseEvent<HTMLElement, MouseEvent>,
+    data: SearchProps
   ) => {
-    setSearchValue(data.value);
+    // setSearchValue(data.value as SetStateAction<string>);
+
+    if (vendorsList) {
+      for (let i = 0; i < vendorsList.length; i++) {
+        if (data.value === vendorsList[i].Name) {
+          let newResult = [...searchResult, vendorsList[i]];
+          setSearchResult(newResult);
+          console.log("found: " + JSON.stringify(vendorsList[i]));
+        }
+      }
+    }
+
+    // console.log(vendorsList);
   };
 
   const searchEnterHandler = () => {};
+  //name, address, and picture
 
   return (
     <Menu.Item className={styles.searchBox}>
-      <Search input={inputBox} size={"small"} />
+      <Search
+        input={inputBox}
+        size={"small"}
+        onSearchChange={onSearchChange}
+        results={[
+          {
+            title: searchResult[0]?.Name,
+            description: searchResult[0]?.BusinessAddress,
+          },
+        ]}
+      />
       {/* <Field>
         {({ form: {dirty, valid } }) => (
         )}
