9bcd12ed8351d90ecee3f6c00a771febb443b7eb
Makoto <makoto@emurasoft.com>
2022-02-14 11:21:24 -0800

    query comments

--- app/src/api.ts
+++ app/src/api.ts
@@ -218,9 +218,11 @@ export const apiSlice = createApi({
     version: builder.query<string, void>({
       query: () => `/version`,
     }),
+    // Gets all vendors.
     vendors: builder.query<Vendor[], void>({
-      query: (id) => `/vendors`,
+      query: () => `/vendors`,
     }),
+    // Gets vendor with specified ID.
     vendor: builder.query<Vendor, string>({
       query: (id) => `/vendors/${encode(id)}`,
     }),
