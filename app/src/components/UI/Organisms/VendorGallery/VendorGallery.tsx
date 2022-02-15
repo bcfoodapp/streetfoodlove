0cefa4990be7435ae4acdb1fe0f7b202a7b8f988
Makoto <makoto@emurasoft.com>
2022-02-15 08:21:18 -0800

    styling

--- app/src/components/UI/Organisms/VendorGallery/VendorGallery.tsx
+++ app/src/components/UI/Organisms/VendorGallery/VendorGallery.tsx
@@ -1,7 +1,7 @@
 import { Photo } from "../../../../api";
 import React from "react";
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
-import { Image } from "semantic-ui-react";
+import { Image, Segment } from "semantic-ui-react";
 
 interface Props {
   photos: Photo[];
@@ -12,33 +12,41 @@ interface Props {
  */
 export default ({ photos }: Props): React.ReactElement => {
   return (
-    <SimpleReactLightbox>
-      <SRLWrapper
-        options={{
-          settings: {
-            slideAnimationType: "slide",
-          },
-          buttons: {
-            showAutoplayButton: false,
-            showDownloadButton: false,
-            showThumbnailsButton: false,
-          },
-        }}
-      >
-        <Image.Group size="small">
-          {photos.map((photo, i) => (
-            <a
-              href={`https://streetfoodlove.s3.us-west-2.amazonaws.com/${photo.ID}.jpg`}
-            >
-              <Image
+    <Segment style={{ width: "100%" }}>
+      <SimpleReactLightbox>
+        <SRLWrapper
+          options={{
+            settings: {
+              slideAnimationType: "slide",
+            },
+            buttons: {
+              showAutoplayButton: false,
+              showDownloadButton: false,
+              showThumbnailsButton: false,
+            },
+          }}
+        >
+          <Image.Group>
+            {photos.map((photo, i) => (
+              <a
+                href={`https://streetfoodlove.s3.us-west-2.amazonaws.com/${photo.ID}.jpg`}
                 key={i}
-                src={`https://streetfoodlove.s3.us-west-2.amazonaws.com/${photo.ID}.jpg`}
-                alt={photo.Text}
-              />
-            </a>
-          ))}
-        </Image.Group>
-      </SRLWrapper>
-    </SimpleReactLightbox>
+              >
+                <Image
+                  src={`https://streetfoodlove.s3.us-west-2.amazonaws.com/${photo.ID}.jpg`}
+                  alt={photo.Text}
+                  style={{
+                    height: 150,
+                    margin: 0,
+                    marginLeft: 2,
+                    marginRight: 2,
+                  }}
+                />
+              </a>
+            ))}
+          </Image.Group>
+        </SRLWrapper>
+      </SimpleReactLightbox>
+    </Segment>
   );
 };
