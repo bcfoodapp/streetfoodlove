a19cd09dfd0202ba7c0e75729e22dc49029630c4
czhou578 <colizu2020@gmail.com>
2022-02-15 14:28:13 -0800

    sidebar closes

--- app/src/components/UI/Molecules/LandingPageSidebar/LandingPageSidebar.tsx
+++ app/src/components/UI/Molecules/LandingPageSidebar/LandingPageSidebar.tsx
@@ -10,12 +10,15 @@ const LandingPageSidebar: React.FC<{setVisible: (param) => void, visible: boolea
       animation="overlay"
       icon="labeled"
       inverted
-      onHide={() => setVisible(false)}
+      // onHide={() => setVisible(true)}
       vertical
       visible={visible}
       width="wide"
       className={styles.sidebar}
     >
+      <Button icon onClick={() => setVisible(false)} className={styles.closeIcon}>
+        <Icon name="close" size="big" color="grey"/>
+      </Button>
       <Menu.Item as="div">
         <Icon name='search' />
         <h3>
