de5137c4f56f9688cfbdc588aa4055e45887f02e
Makoto <makoto@emurasoft.com>
2022-02-15 22:06:57 -0800

    dashboard

--- app/src/components/UI/Pages/VendorDashboard.tsx
+++ app/src/components/UI/Pages/VendorDashboard.tsx
@@ -1,5 +1,4 @@
 import { Container, Card, Icon, Header } from "semantic-ui-react";
-import HeaderBar from "../Molecules/HeaderBar/HeaderBar";
 import styles from "./vendordashboard.module.css";
 
 const VendorDashBoard: React.FC = () => {
@@ -13,44 +12,34 @@ const VendorDashBoard: React.FC = () => {
           <Icon name="setting" size="huge" className={styles.icon} />
           <Card.Content className={styles.content}>
             <Card.Header>Customize Vendor Page</Card.Header>
+            <Card.Description>Customize existing vendor pages</Card.Description>
+          </Card.Content>
+        </Card>
+        <Card className={styles.card}>
+          <Icon name="setting" size="huge" className={styles.icon} />
+          <Card.Content className={styles.content}>
+            <Card.Header>Upload pictures</Card.Header>
             <Card.Description>
-              Customize existing vendor pages!
+              Upload pictures to your page or update previously uploaded
+              pictures
             </Card.Description>
           </Card.Content>
-          {/* <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              10 Friends
-            </a>
-          </Card.Content> */}
         </Card>
         <Card className={styles.card}>
           <Icon name="line graph" size="huge" className={styles.icon} />
           <Card.Content className={styles.content}>
             <Card.Header>Trends</Card.Header>
             <Card.Description>
-              Analyze your business performance!
+              Analyze your business performance
             </Card.Description>
           </Card.Content>
-          {/* <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              10 Friends
-            </a>
-          </Card.Content> */}
         </Card>
         <Card className={styles.card}>
           <Icon name="write" size="huge" className={styles.icon} />
           <Card.Content className={styles.content}>
-            <Card.Header>New Reviews!</Card.Header>
-            <Card.Description>Look at recent reviews!</Card.Description>
+            <Card.Header>New Reviews</Card.Header>
+            <Card.Description>Look at recent reviews</Card.Description>
           </Card.Content>
-          {/* <Card.Content extra>
-            <a>
-              <Icon name='user' />
-              10 Friends
-            </a>
-          </Card.Content> */}
         </Card>
       </Card.Group>
     </Container>
