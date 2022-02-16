80ca992ce26ef825acb2f51fa6704026c5a9d6dc
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2022-02-15 17:15:13 -0800

    Search vendor Functionality (#225)
    * commit
    
    * changed styling of specific pages to make it more responsive
    
    * npx pretty
    
    * modified searchbox
    
    * resized css
    
    * npx pretty, results showing
    
    * search working properly

--- app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
+++ app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
@@ -1,26 +1,86 @@
-import React from "react";
-import { Container, Input, Menu } from "semantic-ui-react";
+import React, { ChangeEvent, SetStateAction, useState } from "react";
+import {
+  Container,
+  Input,
+  InputOnChangeData,
+  Menu,
+  Search,
+  SearchProps,
+} from "semantic-ui-react";
 import styles from "./searchbox.module.css";
 import Buttons from "../Button/Buttons";
+import { useVendorsQuery, Vendor } from "../../../../api";
+import { Field } from "formik";
 
 /**
  * This is the searchbox for the header
  */
 
+const inputBox = (
+  <Input
+    icon={
+      <Buttons enter color={"green"}>
+        Enter
+      </Buttons>
+    }
+    placeholder="Search..."
+    focus
+    size={"small"}
+    className={styles.inputBox}
+  />
+);
+
 export const SearchBox: React.FC = () => {
-  const searchEnterHandler = () => {};
+  const [searchResult, setSearchResult] = useState<Vendor[]>([]);
+  const { data: vendorsList } = useVendorsQuery();
+  console.log(vendorsList);
+
+  const onSearchChange = (
+    event: React.MouseEvent<HTMLElement, MouseEvent>,
+    data: SearchProps
+  ) => {
+    if (data.value?.length === 0) {
+      setSearchResult([]);
+      return;
+    }
+
+    if (vendorsList) {
+      let search = data.value;
+      let condition = new RegExp(search as string);
+
+      let result = vendorsList.filter((element) => {
+        return condition.test(element.Name);
+      });
+
+      let resultArray: Vendor[] = [];
+
+      for (let obj of result) {
+        let tempObject = {
+          title: obj.Name,
+          description: obj.BusinessAddress,
+          ...obj,
+        };
+
+        resultArray.push(tempObject);
+      }
+
+      setSearchResult(resultArray);
+    }
+  };
 
   return (
     <Menu.Item className={styles.searchBox}>
-      <Input
-        icon={
-          <Buttons enter color={"green"} clicked={searchEnterHandler}>
-            Enter
-          </Buttons>
-        }
-        placeholder="Search..."
-        focus
+      <Search
+        input={inputBox}
+        size={"small"}
+        onSearchChange={onSearchChange}
+        results={searchResult}
+        showNoResults
       />
+      {/* <Field>
+        {({ form: {dirty, valid } }) => (
+        )}
+      </Field> */}
     </Menu.Item>
   );
 };
