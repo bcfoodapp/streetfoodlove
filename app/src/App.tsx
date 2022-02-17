359d78271adaff809d01ca139ed9512e75acbe4d
Makoto <makoto@emurasoft.com>
2022-02-16 17:54:37 -0800

    rename

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ import BusinessGuideArticle from "./components/UI/Molecules/BusinessGuideGUI/Bus
 import HeaderBar from "./components/UI/Molecules/HeaderBar/HeaderBar";
 import MessageError from "./components/UI/Atoms/Message/MessageError";
 import LoginWrapper from "./components/UI/Pages/Login";
-import VendorPhotosEditor from "./components/UI/Pages/VendorPhotosUploader";
+import VendorPhotosUploader from "./components/UI/Pages/VendorPhotosUploader";
 
 function App(): React.ReactElement {
   return (
@@ -38,7 +38,7 @@ function App(): React.ReactElement {
           <Route path="/vendor-dashboard" element={<VendorDashBoard />} />
           <Route
             path="/vendor-dashboard/photos"
-            element={<VendorPhotosEditor />}
+            element={<VendorPhotosUploader />}
           />
           <Route path="/edit-vendor-page" element={<EditVendorPage />} />
           <Route path="/guides/:ID" element={<BusinessGuideArticle />} />
