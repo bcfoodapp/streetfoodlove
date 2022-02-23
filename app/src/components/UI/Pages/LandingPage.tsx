9b0ac3669c3c59c2104aa0296ff323e0bde10cbe
czhou578 <colizu2020@gmail.com>
2022-02-22 22:43:19 -0800

    npx pretty

--- app/src/components/UI/Pages/LandingPage.tsx
+++ app/src/components/UI/Pages/LandingPage.tsx
@@ -1,10 +1,8 @@
-import { Button, Container, Icon, Sidebar } from "semantic-ui-react";
+import { Container, Sidebar } from "semantic-ui-react";
 import Map from "../Atoms/Map";
-import styles from "./landingpage.module.css";
 import config from "../../../configuration.json";
 import { useVersionQuery } from "../../../api";
 import LandingPageSidebar from "../Molecules/LandingPageSidebar/LandingPageSidebar";
-import React from "react";
 
 export const LandingPage = () => {
   const { data: version } = useVersionQuery();
