4f5141bc9265c518cbaccdac7cfee1d036734965
Colin Zhou <42430035+czhou578@users.noreply.github.com>
2022-02-25 10:46:06 -0800

    Optimize search to account for recent searches (#266)
    * commit
    
    * changed styling of specific pages to make it more responsive
    
    * npx pretty
    
    * added vendor title
    
    * npx pretty
    
    * imported sidebar
    
    * menu sliding out
    
    * added icons
    
    * sidebar closes
    
    * resized map
    
    * commit
    
    * npx pretty
    
    * fixed map and got rid of input box
    
    * commit
    
    * fixed map
    
    * deleted button toggle on map
    
    * added results header and fixed positioning
    
    * npx pretty
    
    * commit
    
    * use redux to handle sidebar open/close state
    
    * npx pretty
    
    * added array for recent search result
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * npx pretty

--- app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
+++ app/src/components/UI/Atoms/SearchBox/SearchBox.tsx
@@ -1,31 +1,52 @@
-import React, { ChangeEvent, SetStateAction, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Input, Menu, Search, SearchProps } from "semantic-ui-react";
 import styles from "./searchbox.module.css";
 import Buttons from "../Button/Buttons";
 import { useVendorsQuery, Vendor } from "../../../../api";
 import { Field } from "formik";
+import { useAppDispatch } from "../../../../store";
+import { showSideBar } from "../../../../store";
 
 /**
  * This is the searchbox for the header
  */
 
-const inputBox = (
-  <Input
-    icon={
-      <Buttons enter color={"green"}>
-        Enter
-      </Buttons>
-    }
-    placeholder="Search..."
-    focus
-    size={"small"}
-    className={styles.inputBox}
-  />
-);
-
 export const SearchBox: React.FC = () => {
   const [searchResult, setSearchResult] = useState<Vendor[]>([]);
+  const [recentSearchResult, setRecentSearchResult] = useState<Vendor[]>([]);
   const { data: vendorsList } = useVendorsQuery();
+  const inputRef = useRef<any>(null);
+  const dispatch = useAppDispatch();
+
+  const enterQueryHandler = () => {
+    let resultSet = new Set([
+      inputRef.current.props.value,
+      ...recentSearchResult,
+    ]);
+
+    let array: Vendor[] = [];
+
+    if (vendorsList !== undefined) {
+      for (const vendor of vendorsList) {
+        if (
+          vendor.Name === inputRef.current.props.value &&
+          resultSet.has(vendor.Name)
+        ) {
+          let obj = {
+            title: vendor.Name,
+            description: vendor.BusinessAddress,
+            ...vendor,
+          };
+
+          array.push(obj);
+        }
+      }
+    }
+
+    setRecentSearchResult([...array, ...recentSearchResult]);
+
+    dispatch(showSideBar());
+  };
 
   const onSearchChange = (
     event: React.MouseEvent<HTMLElement, MouseEvent>,
@@ -39,23 +60,41 @@ export const SearchBox: React.FC = () => {
     if (vendorsList) {
       let search = data.value;
       let condition = new RegExp(search as string);
+      // console.log(condition);
+      let resultArray: Vendor[] = [];
 
-      let result = vendorsList.filter((element) => {
+      let filteredResult = vendorsList.filter((element) => {
+        //filter all vendors from vendors list who matches regex expression
         return condition.test(element.Name);
       });
 
-      let resultArray: Vendor[] = [];
+      let recentSearchFilteredResult = recentSearchResult.filter((element) => {
+        //filter all vendors from recent search who matches regex expr.
+        return condition.test(element.Name);
+      });
 
-      for (let obj of result) {
+      // console.table(recentSearchFilteredResult);
+
+      for (let i = 0; i < filteredResult.length; i++) {
+        //loop through all vendors that pass the regex filter
+
         let tempObject = {
-          title: obj.Name,
-          description: obj.BusinessAddress,
-          ...obj,
+          title: filteredResult[i].Name,
+          description: filteredResult[i].BusinessAddress,
+          ...filteredResult[i],
         };
 
-        resultArray.push(tempObject);
+        if (
+          //if the tempobject has no matches in recentsearch result, then push to the result array
+          !recentSearchResult.some(
+            (element) => element.Name === tempObject.title
+          )
+        ) {
+          resultArray.push(tempObject);
+        }
       }
 
+      resultArray.unshift(...recentSearchFilteredResult); //add filtered recentsearchresult to the front of reusltarray
       setSearchResult(resultArray);
     }
   };
@@ -63,7 +102,20 @@ export const SearchBox: React.FC = () => {
   return (
     <Menu.Item className={styles.searchBox}>
       <Search
-        input={inputBox}
+        input={
+          <Input
+            icon={
+              <Buttons enter color={"green"} clicked={enterQueryHandler}>
+                Enter
+              </Buttons>
+            }
+            placeholder="Search..."
+            focus
+            size={"small"}
+            className={styles.inputBox}
+            ref={inputRef}
+          />
+        }
         size={"small"}
         onSearchChange={onSearchChange}
         results={searchResult}
