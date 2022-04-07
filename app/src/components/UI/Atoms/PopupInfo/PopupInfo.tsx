d56ac9d0048bb7a40cfeee216d5f16a56bd81183
Makoto <makoto@emurasoft.com>
2022-04-07 09:37:42 -0700

    show vendor image on popup

--- app/src/components/UI/Atoms/PopupInfo/PopupInfo.tsx
+++ app/src/components/UI/Atoms/PopupInfo/PopupInfo.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./popupInfo.module.css";
-import { Vendor } from "../../../../api";
+import { usePhotosByLinkIDQuery, Vendor } from "../../../../api";
 import { Link } from "react-router-dom";
+import { Container, Image } from "semantic-ui-react";
+import { s3Prefix } from "../../../../aws";
 
 interface Props {
   vendor: Vendor;
@@ -11,35 +13,22 @@ interface Props {
  * This component is for storing the vendor information in the popups that appear on the map
  */
 export default function PopupInfo({ vendor }: Props): React.ReactElement {
+  const { data: photos } = usePhotosByLinkIDQuery(vendor.ID);
+
   return (
-    <div>
-      <div className={styles.header}>
-        <div className={styles.vendorName}>
-          <div>
-            <span className={styles.title}>
-              <Link to={`/vendors/${vendor.ID}`}>{vendor.Name}</Link>
-            </span>
-          </div>
-          <div className={styles.firstImage}>
-            <img
-              src="https://media.istockphoto.com/vectors/five-stars-rating-vector-id925469766?k=20&m=925469766&s=170667a&w=0&h=Z-e1FuriS6-RmQ4YRuZbPxaWFEWm41D9fiTaNCQIGy4="
-              alt="cannot be displayed"
-            />
-          </div>
-        </div>
-        <div className={styles.secondImage}>
-          <img
-            src="https://s3-media0.fl.yelpcdn.com/bphoto/L96pd4xrj0l_AvDeO8I0JQ/348s.jpg"
-            alt="cannot be displayed"
-          />
-        </div>
-      </div>
-      <div className="sampleReviewWrap">
-        <h3>Review By Colin Zhou</h3>
-        <pre>
-          This was the tastiest spot that I've ever eaten at for dinner!
-        </pre>
-      </div>
-    </div>
+    <>
+      <Container className={styles.header}>
+        <Container className={styles.title}>
+          <Link to={`/vendors/${vendor.ID}`}>{vendor.Name}</Link>
+        </Container>
+      </Container>
+      {photos && photos.length > 0 ? (
+        <Image
+          src={s3Prefix + photos[0].ID}
+          alt={photos[0].Text}
+          style={{ height: 200 }}
+        />
+      ) : null}
+    </>
   );
 }
