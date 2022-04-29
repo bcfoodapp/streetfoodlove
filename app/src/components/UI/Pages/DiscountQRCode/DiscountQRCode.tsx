694ff320ae93177dbdf61c770f54f1d8e22d320c
Makoto <makoto@emurasoft.com>
2022-04-29 07:28:38 -0700

    QR code

--- app/src/components/UI/Pages/DiscountQRCode/DiscountQRCode.tsx
+++ app/src/components/UI/Pages/DiscountQRCode/DiscountQRCode.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import QRCode from "react-qr-code";
 import { Link, useParams } from "react-router-dom";
 import { useDiscountQuery } from "../../../../api";
+import { Container, Divider, Header } from "semantic-ui-react";
 
 export default (): React.ReactElement => {
   const id = useParams().ID as string;
@@ -9,8 +11,18 @@ export default (): React.ReactElement => {
   const discountURL = `/vendor-dashboard/validate-discount/${discount?.Secret}`;
 
   return (
-    <>
-      <Link to={discountURL}>{discountURL}</Link>
-    </>
+    <Container>
+      <Header as="h1">Discount QR Code</Header>
+      {discount ? (
+        <>
+          <Container textAlign="center">
+            <QRCode value={discountURL} />
+          </Container>
+
+          <p>URL for testing:</p>
+          <Link to={discountURL}>{discountURL}</Link>
+        </>
+      ) : null}
+    </Container>
   );
 };
