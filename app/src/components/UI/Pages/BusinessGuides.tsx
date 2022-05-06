f497b63d4948768d063325cbf8e94a4490c5a98b
Makoto <makoto@emurasoft.com>
2022-05-05 21:35:21 -0700

    guide preview

--- app/src/components/UI/Pages/BusinessGuides.tsx
+++ app/src/components/UI/Pages/BusinessGuides.tsx
@@ -2,6 +2,21 @@ import { Container, Header, Card } from "semantic-ui-react";
 import styles from "./businessGuide.module.css";
 import { useGuidesQuery } from "../../../api";
 
+// Returns the first n words of string.
+export function firstWords(s: string, numberOfSpaces: number): string {
+  let currentIndex = 0;
+  for (let i = 0; i < numberOfSpaces; i++) {
+    const index = s.indexOf(" ", currentIndex);
+    if (index === -1) {
+      currentIndex = s.length + 1;
+      break;
+    }
+    currentIndex = index + 1;
+  }
+
+  return s.substring(0, currentIndex - 1);
+}
+
 const BusinessGuides: React.FC = () => {
   const { data: guides } = useGuidesQuery();
 
@@ -17,7 +32,7 @@ const BusinessGuides: React.FC = () => {
             {guides?.slice(0, 4).map((guide, i) => (
               <Card
                 header={`Guide ${i}`}
-                description={`${guide.Guide.substring(0, 50)}...`}
+                description={`${firstWords(guide.Guide, 15)}...`}
               />
             ))}
           </Card.Group>
@@ -25,10 +40,10 @@ const BusinessGuides: React.FC = () => {
         <Container className={styles.newCards}>
           <Header as="h2">Recently Posted</Header>
           <Card.Group itemsPerRow={2}>
-            {guides?.slice(0, 4).map((guide, i) => (
+            {guides?.map((guide, i) => (
               <Card
                 header={`Guide ${i}`}
-                description={`${guide.Guide.substring(0, 50)}...`}
+                description={`${firstWords(guide.Guide, 15)}...`}
               />
             ))}
           </Card.Group>
