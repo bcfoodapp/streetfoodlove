34a0490e0c62050f82df449fc2d7d68dfbfdd22f
Makoto <makoto@emurasoft.com>
2022-05-08 05:59:47 -0700

    addressToCoordinates()

--- app/src/aws.ts
+++ app/src/aws.ts
@@ -2,6 +2,10 @@ import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { FromTemporaryCredentialsOptions } from "@aws-sdk/credential-providers/dist-types";
 import { fromTemporaryCredentials } from "@aws-sdk/credential-providers";
 import { AWSCredentials } from "./api";
+import {
+  LocationClient,
+  SearchPlaceIndexForTextCommand,
+} from "@aws-sdk/client-location";
 
 function convertCredentials(
   c: AWSCredentials
@@ -43,3 +47,32 @@ export async function uploadToS3(
 
 // The S3 bucket URL
 export const s3Prefix = "https://streetfoodlove.s3.us-west-2.amazonaws.com/";
+
+// Returns the coordinates of the best match for the given address.
+export async function addressToCoordinates(
+  credentials: AWSCredentials,
+  text: string
+) {
+  const client = new LocationClient({
+    region: "us-west-2",
+    credentials: fromTemporaryCredentials({
+      clientConfig: {
+        region: "us-west-2",
+      },
+      masterCredentials: convertCredentials(credentials),
+      params: {
+        RoleArn: "arn:aws:iam::907229944921:role/SFLLocationRole",
+      },
+    }),
+  });
+
+  const command = new SearchPlaceIndexForTextCommand({
+    IndexName: "sfl-place",
+    Text: text,
+    BiasPosition: [-122.14819, 47.584401],
+    MaxResults: 1,
+  });
+
+  const output = await client.send(command);
+  console.log(output);
+}
