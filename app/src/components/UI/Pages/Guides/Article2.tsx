375d1564ef58227b8970c2dd5d95cc8737b49db7
wenhuan tan <46391831+Wenhuan2516@users.noreply.github.com>
2022-05-18 18:20:20 -0700

    Add the first guide article- How to start a food cart business? (#432)
    * add article 1 - how to start a food cart business?
    
    * change the function name

---
+++ app/src/components/UI/Pages/Guides/Article2.tsx
@@ -0,0 +1,298 @@
+import { Container, Header } from "semantic-ui-react";
+
+export default function AddArticle2() {
+  return (
+    <Container>
+      <Header as="h2">How to Buy a Food Truck</Header>
+      <p>
+        Purchasing a kitchen-equipped truck is the most expensive and
+        time-consuming part of starting a food truck business. You will need to
+        find a food truck manufacturer you trust, establish clear communication,
+        and customize your food truck to meet both your personal needs and
+        regional requirements. To make buying a food truck less intimidating, we
+        have created a comprehensive guide to the food truck purchasing and
+        personalizing process. We will explain the average food truck costs and
+        help you determine whether a new, used, or leased food truck is right
+        for you.
+      </p>
+      <h3>How Much Does a Food Truck Cost?</h3>
+      <p>
+        On average, most food truck entrepreneurs spend between $70,000 and
+        $80,000 on their food truck. The total price of buying a food truck can
+        amount anywhere from $50,000 to $250,000 depending on how you acquire
+        the truck, customize it for your needs, and your local permit and
+        licensing requirements. To help you narrow down how much you should
+        expect to spend on your food truck, we break down the average cost of
+        new, used, and leased food trucks below.
+      </p>
+      <p>
+        <em>How Much Does a New Food Truck Cost?</em>New food trucks typically
+        cost between $100,000-$175,000.
+      </p>
+      <p>
+        <em>How Much Does a Used Food Truck Cost?</em>-Used food trucks
+        typically cost between $50,000-$100,000.
+      </p>
+      <p>
+        <em>How Much Does Leasing a Food Truck Cost?</em>-Leasing a food truck
+        typically costs between $2,000-$3,000 each month.
+      </p>
+      <h3>How to Own a Food Truck</h3>
+      <p>
+        The first step in owning a food truck is assessing the financial
+        obligation by writing a food truck business plan. Begin by researching
+        your target market, local ordinances, licensing and permit demands,
+        parking permit regulations, and what type of equipment your menu
+        requires.Most food truck entrepreneurs need to apply for financing or a
+        loan to offset food truck startup costs.
+      </p>
+      <p>
+        Before heading to your bank, know your credit score and based on your
+        food truck business plan, know how much owning a food truck business
+        will cost you.Once you are approved for a loan, you will know your
+        budget and can start shopping for a food truck.
+      </p>
+      <p>There are three paths to owning a food truck business: </p>
+      <ul>
+        <li>buying a new food truck</li>
+        <li>buying a used food truck</li>
+        <li>leasing a food truck</li>
+      </ul>
+      <h3>What Is Better, to Buy a New or Used Food Truck?</h3>
+      <p>
+        One of the biggest decisions of buying a food truck is deciding whether
+        to purchase it new or used. While there are advantages and disadvantages
+        of each choice, it ultimately comes down to what your budget is and how
+        much you can afford upfront. We'll go over the pros and cons of buying
+        new and used food trucks, so you can make the best decision for your
+        financial status.
+      </p>
+      <h4>Buying a New Food Truck</h4>
+      <p>
+        If you have the money, buying a new food truck is a worthwhile
+        investment that will help you avoid costly repairs down the line.
+        Discover the advantages and disadvantages of buying a new food truck
+        below.
+      </p>
+      <h4>New Food Truck Advantages</h4>
+      <ul>
+        <li>Custom-tailored to your needs</li>
+        <li>No wear and tear or undisclosed damages</li>
+        <li>Reduces the risk of costly breakdowns and major repairs</li>
+        <li>Usually have great warranties</li>
+        <li>Fresh, clean, and polished appearances</li>
+        <li>New Food Truck Disadvantages</li>
+        <li>Large financial investment</li>
+        <li>Customization requires longer lead times</li>
+        <li>
+          High initial expense makes it hard to make future changes if you
+          quickly outgrow the truck's capacity
+        </li>
+      </ul>
+      <h4>Buying a Used Food Truck</h4>
+      <p>
+        If you want to own your food truck but are hesitant to invest in a brand
+        new vehicle, buying a used food truck is a great alternative. Learn
+        about the advantages and disadvantages of buying a used food truck
+        below.
+      </p>
+      <h4>Used Food Truck Advantages</h4>
+      <ul>
+        <li>Initially cheaper than new trucks</li>
+        <li>
+          Pre-done retrofitting allows you to see if the kitchen layout works
+          for you
+        </li>
+        <li>Equipment is already installed</li>
+        <li>Cuts the decision-making process down</li>
+      </ul>
+      <h4>Used Food Truck Disadvantages</h4>
+      <ul>
+        <li>Higher risk of costly repairs and maintenance</li>
+        <li>Unknown wear and tear</li>
+        <li>Lower life expectancy</li>
+        <li>
+          You may have trouble finding a vendor willing to customize it-because
+          they don't know what was done previously
+        </li>
+      </ul>
+      <h3>Lease a Food Truck</h3>
+      <p>
+        Purchasing a kitchen-equipped truck is the most expensive part of
+        becoming a food truck operator, so many entrepreneurs circumnavigate
+        this major up-front cost by leasing their food truck. Making smaller
+        payments over time allows you to get a brand new, high-end food truck
+        without a large initial fee. The leasing route might be right for you if
+        you're new to the foodservice industry or want to see if your brick and
+        mortar does well as a food truck. One disadvantage of leasing a food
+        truck is you may have to return the truck when your lease ends. If your
+        food truck business is doing well, you'll endure a stressful transition
+        to a new truck, lose operational time, or be forced to close your
+        thriving business. We strongly recommend choosing a food truck lease
+        agreement with lease renewal or lease-to-own options.
+      </p>
+      <h3>Where To Buy a Food Truck</h3>
+      <p>
+        Once you know your budget and decide whether you want a new, used, or
+        leased food truck, you'll need to find a reputable food truck dealer.
+        While you can purchase food trucks from private parties and sites like
+        Craigslist or eBay, it's important to recognize the high risk of this
+        choice. When investing an immense sum of money into the foundation of
+        your business, you want to know every detail about your future food
+        truck.
+      </p>
+      <p>
+        The website Food Truck Empire provides a list of reputable dealers and
+        helps you find a dealer in your state. Also, many entrepreneurs ask the
+        owner of their favorite food truck where they purchased their vehicle.
+        Asking food truck owners for recommendations provides the opportunity to
+        inquire about how well their truck runs and if they had a positive
+        experience with its vendor.Additionally, we've created a list of
+        reputable food truck manufactures below. These dealers offer a selective
+        inventory of food trucks, and most offer leasing options. You can fully
+        customize your food truck when purchasing from any of these sites.
+      </p>
+      <h3>Design Your Own Food Truck</h3>
+      <p>
+        Customizing your food truck is both the most stressful and most
+        intriguing part of buying a food truck. If you go with a reputable
+        manufacturer, they'll take most of the stress off your shoulders and
+        bring your vision to life. Founder of Prestige Food Trucks, Jeremy
+        Adams, provided some great insight on how a typical purchasing process
+        goes. While a lot more communication goes into the purchase than what is
+        listed below, these five steps provide a general outline of the process.
+      </p>
+      <ul>
+        <li>
+          A customer will call or email the company they want to purchase from
+          with an inquiry.
+        </li>
+        <li>
+          The customer receives a response that typically asks them what their
+          equipment needs, and budget are, what city they'll be operating in,
+          and what type of food they'll be serving.
+        </li>
+        <li>
+          Once this information is provided, a company will usually send out
+          custom blueprint options to which customers can make any adjustments.
+        </li>
+        <li>
+          After this is completed, a company typically works on a 2-3-month lead
+          time before shipping the food truck. Many manufacturers can ship
+          nationally or internationally.
+        </li>
+        <li>
+          Once a customer receives their purchase, they must obtain any permits
+          needed before they can hit the streets selling their specialty
+          cuisine.
+        </li>
+      </ul>
+      <h3>How to Customize a Food Truck</h3>
+      <p>
+        The main determining factor in how you should layout of your food truck
+        is the cuisine you're offering. While the most common food truck items
+        are flat grills, countertop fryers, food warmers, refrigerators, and
+        freezers, each truck will differ. For example, a food truck specializing
+        in pizza needs a pizza oven and possibly an extra generator or propane
+        tank, whereas a coffee truck benefits from an extra supply of hot water.
+        Also, as you customize your food truck to your menu, make sure your
+        layout provides enough space for other pieces of essential food truck
+        equipment.
+      </p>
+      <h4>Food Truck Size</h4>
+      <p>
+        The average food truck is 16 feet long and seven feet wide, but they can
+        range from 10 to 26 feet long depending on their operational needs. Make
+        sure you choose a vehicle that's large enough to accommodate your
+        kitchen layout, ingredient storage requirements, and staff members.
+        After assessing your needs, decide whether your mobile food business
+        requires a food truck, food trailer, or a food cart. Some companies can
+        transform anything, from step van chassis and sprinter vans to passenger
+        vans and RVs, into fully functioning food trucks.
+      </p>
+      <h4>Food Truck Fuel</h4>
+      <p>
+        Decide whether you want to go with a gas or diesel engine for your food
+        truck. There are many pros and cons to each type, so it comes down to
+        preference. Most food truck owners choose diesel engines for their
+        power, but you have to be prepared for that slightly larger initial
+        investment.While gasoline is cheaper than diesel fuel, diesel has a
+        greater fuel economy. Gas engines will run more quietly and have a less
+        noticeable odor, whereas diesel engines provide you with unmatched
+        power. If properly maintained, a diesel engine can easily go for 300,000
+        miles. When engines require maintenance, gas operating engines are
+        typically cheaper to repair than diesel. However, since diesel engines
+        last longer, you'll get a better resale price if you ever decide to sell
+        your food truck.
+      </p>
+      <h4>Food Truck Power Supply</h4>
+      <p>
+        Portable diesel generators are the typical power source for food trucks
+        and trailers. Diesel-powered generators withstand constant heavy use and
+        have long lifespans. If you live in a sunny location, you can
+        sustainably power your food truck by installing solar panels to the
+        roof. Another food truck power supply option is to use Liquefied
+        Petroleum Gas cylinders.
+      </p>
+      <h4>Food Truck Special Features</h4>
+      <p>
+        Finally, when customizing your food truck, you get to pick the special
+        features that will make it your own unique restaurant on wheels. Whether
+        you're adding special equipment to the interior of the truck to make
+        food prep more efficient, or you're adding aesthetic details to the
+        exterior to make your truck more visually appealing to customers, a
+        reputable dealer will work with your vision. Check out some beneficial
+        accessories you can add to your truck below.
+      </p>
+      <p>
+        <em>Security Awning</em>-This locking security awning will protect the
+        contents of your food truck when it's not in use. Security awnings are
+        usually a sliding piece that covers windows, doors, and any external
+        TVs, speakers, or other devices to protect them from vandalism. Use the
+        awning as an extra branding space by customizing it to match your
+        truck's design.
+      </p>
+      <p>
+        <em>Flat Panel TV </em>- Add a flat panel television to the exterior of
+        your food truck so you can market your menu, specials, locations, and
+        other important information. You can also use it to play music.
+      </p>
+      <p>
+        <em>Speakers</em> - By adding speakers, you can announce when orders are
+        ready and entertain your long line of customers with music.
+      </p>
+      <p>
+        <em>Retractable Awning</em> - Manual or electric awnings shelter
+        customers while they wait at your serving window and create a slight
+        sound barrier that allows workers to hear customers' orders more
+        clearly. Longer awnings protect patrons from the rain, snow, or glaring
+        sun while they're waiting to order, which prevents lost business due to
+        long, uncomfortable wait times.
+      </p>
+      <p>
+        <em>Misting System</em> - This feature is perfect for the summer months
+        or for food trucks in areas where the climate is hot year-round. Food
+        truck operators usually install misting systems above their exterior
+        serving window to keep customers cool while they wait.
+      </p>
+      <p>
+        <em>Vinyl Wrap</em> - Many businesses have designers who work with
+        clients to create a personal logo or image. They then transfer the logo
+        onto a vinyl wrapping that goes around the entire truck for branding.
+        These wrappings can include graphics, menus, business names, and any
+        other advertising materials, and they will include the proper cutouts
+        for windows and serving windows.
+      </p>
+      <p>
+        <em>Perforated Window Film</em>- Window perforation allows your logo or
+        decal to continue onto the exterior of your windows. This allows
+        customers to see the image, but also enables employees to still see
+        clearly out the window.
+      </p>
+      <p>
+        <em>Dual-Service </em>- Many truck owners want to have service stations
+        on both sides of the truck so they can maximize volume and service.
+      </p>
+    </Container>
+  );
+}
